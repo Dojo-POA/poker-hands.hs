3d39e71d40aca713b80a524189e6485bd8acc6c6
Bruno Tavares <bruno.exz@gmail.com>
2013-10-02 20:28:15 -0300

    card face value is known

--- src/Dojo/main.hs
+++ src/Dojo/main.hs
@@ -1,4 +1,22 @@
 module Dojo.Main where
+import Data.Char(digitToInt)
 
-example :: Int -> String
-example = show
+type Card = (Char, Char)
+type Hand = [Card]
+
+leftHand = GT
+rightHand = LT
+draw = EQ
+
+faceValue 'A' = 14
+faceValue 'K' = 13
+faceValue 'Q' = 12	
+faceValue 'J' = 11
+faceValue 'T' = 10
+faceValue face = digitToInt face
+
+compareHands hand1 hand2 =
+	compare (faceValue face1) (faceValue face2)
+	where 
+		(face1, _) = head hand1
+		(face2, _) = head hand2
--- test/Dojo/MainSpec.hs
+++ test/Dojo/MainSpec.hs
@@ -5,14 +5,52 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 import Dojo.Main
 
+
+
+
 spec :: Spec
 spec = do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+  describe "High card" $ do
+    it "draw when highest cards are equal" $ do
+      let hand1 = [('4', 'H')]
+      let hand2 = [('4', 'H')]
+      compareHands hand1 hand2 `shouldBe` draw
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
+    it "right highest card wins" $ do
+        let h1 = [('5', 'H')]
+        let h2 = [('6', 'H')]
+        compareHands h1 h2 `shouldBe` rightHand
 
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    it "left highest card wins" $ do
+        let h1 = [('3', 'H')]
+        let h2 = [('2', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand  
+
+    it "A should win K" $ do
+        let h1 = [('A', 'H')]
+        let h2 = [('K', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand
+
+    it "K should win Q" $ do
+        let h1 = [('K', 'H')]
+        let h2 = [('Q', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand
+
+    it "Q should win J" $ do
+        let h1 = [('Q', 'H')]
+        let h2 = [('J', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand
+
+    it "J should win 10" $ do
+        let h1 = [('J', 'H')]
+        let h2 = [('T', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand
+
+    it "10 should win 9" $ do
+        let h1 = [('T', 'H')]
+        let h2 = [('9', 'H')]
+        compareHands h1 h2 `shouldBe` leftHand
+
+
+
+
